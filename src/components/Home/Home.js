2c69a47cacc0283bb69a01d068aef3f0ee129e62
robinNcode <msmrobin518@gmail.com>
2023-01-05 20:57:31 +0600

    readme and home first section upadated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,15 +14,12 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
+                Assalamualaikum!
               </h1>
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
+                <strong className="main-name"> MD Shahin Mia Robin</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,9 +6,9 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
+          "Software Engineer",
+          "Algorithmic Problem Solver",
+          "Enthusiastic Learner",
           "Open Source Contributor",
         ],
         autoStart: true,

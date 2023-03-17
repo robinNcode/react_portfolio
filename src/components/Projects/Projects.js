8be1ddd9ba675bf2516e5b58e7e19946dbf10dae
robinNcode <msmrobin518@gmail.com>
2023-03-17 22:19:08 +0600

    unused element removed

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,16 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
 import ptms from "../../Assets/Projects/ptms.png";
 import rentals from "../../Assets/Projects/rentals.png";
 import pcms from "../../Assets/Projects/pcms.png";
-import saleSoftware from "../../Assets/Projects/sales_software.png";
 
 function Projects() {
   return (

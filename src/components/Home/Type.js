5c3d8dc597ccc190ea29ed3e28e16912f2394401
robinNcode <msmrobin518@gmail.com>
2023-04-17 01:27:32 +0600

    splling mistake

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,7 +7,7 @@ function Type() {
       options={{
         strings: [
           "Software Engineer <br/> Adova Soft.",
-          "Graduated with Bachalor of Science in Computer Science and Engineering from Sonargaon University."
+          "Graduated with Bachelor of Science in Computer Science and Engineering from Sonargaon University."
         ],
         autoStart: true,
         loop: true,

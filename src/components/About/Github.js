bba4166e46f5805e654743670fb7edb6494d1dac
robinNcode <msmrobin518@gmail.com>
2023-04-17 01:21:00 +0600

    message modified

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -6,7 +6,7 @@ function Github() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
-        <strong className="purple">My Coding Calendar</strong>
+      My Working Stats on<strong className="purple"> GitHub</strong>
       </h1>
       <GitHubCalendar
         username="robinNcode"

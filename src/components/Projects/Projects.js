73545389c5fab49dc0ec1a5d485ebf9db5516642
robinNcode <msmrobin518@gmail.com>
2023-04-17 01:18:39 +0600

    All done

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,7 +15,7 @@ function Projects() {
           My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Sharing my thoughts, ideas, experience, and imagination
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">

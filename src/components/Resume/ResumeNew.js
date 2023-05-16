2f87d16595d5568b006eebb8180efa38fc45c2d0
robinNcode <msmrobin518@gmail.com>
2023-05-16 23:40:12 +0600

    Resume Updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/robinNcode/react_portfolio/master/src/Assets/my_cv_09_03_23.pdf";
+  "https://raw.githubusercontent.com/robinNcode/react_portfolio/master/src/Assets/Resume_MsM_Robin.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
